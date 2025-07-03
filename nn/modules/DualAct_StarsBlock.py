e6e2e5f8e10b95692c7f6426cf07db20cad171e9
MarinJH <148436558+MarinJH@users.noreply.github.com>
2025-07-03 18:01:35 +0800

    Add files via upload

---
+++ nn/modules/DualAct_StarsBlock.py
@@ -0,0 +1,52 @@
+
+import torch
+import torch.nn as nn
+from timm.models.layers import DropPath
+from .conv import Conv
+
+
+class ConvBN(torch.nn.Sequential):
+    def __init__(self, in_planes, out_planes, kernel_size=1, stride=1, padding=0, dilation=1, groups=1, with_bn=True):
+        super().__init__()
+        self.add_module('conv', torch.nn.Conv2d(in_planes, out_planes, kernel_size, stride, padding, dilation, groups))
+        if with_bn:
+            self.add_module('bn', torch.nn.BatchNorm2d(out_planes))
+            torch.nn.init.constant_(self.bn.weight, 1)
+            torch.nn.init.constant_(self.bn.bias, 0)
+
+
+class StarsBlock(nn.Module):
+    def __init__(self, dim, mlp_ratio=3, drop_path=0.):
+        super().__init__()
+        self.dwconv = ConvBN(dim, dim, 7, 1, (7 - 1) // 2, groups=dim, with_bn=True)
+        self.post_conv_act = nn.ReLU6()  #
+
+        self.f1 = ConvBN(dim, mlp_ratio * dim, 1, with_bn=False)
+        self.f2 = ConvBN(dim, mlp_ratio * dim, 1, with_bn=False)
+
+        self.hswish = nn.Hardswish()  #
+
+        self.g = ConvBN(mlp_ratio * dim, dim, 1, with_bn=True)
+        self.dwconv2 = ConvBN(dim, dim, 7, 1, (7 - 1) // 2, groups=dim, with_bn=False)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+    def forward(self, x):
+        input = x
+        x = self.dwconv(x)
+        x = self.post_conv_act(x)  #
+
+        x1, x2 = self.f1(x), self.f2(x)
+        x = self.hswish(x1) * x2  #
+
+        x = self.dwconv2(self.g(x))
+        x = input + self.drop_path(x)
+        return x
+
+
+
+
+
+
+
+
+
