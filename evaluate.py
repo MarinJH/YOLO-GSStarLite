dd9d217fc322dd49162a0ee740a9ab6c971ee198
MarinJH <148436558+MarinJH@users.noreply.github.com>
2025-07-03 16:38:46 +0800

    Delete evaluate.py

--- evaluate.py
+++
@@ -1,9 +0,0 @@
-#coding: utf-8
-from ultralytics import YOLO
-import matplotlib
-matplotlib.use( "TkAgg")
-if __name__ == '__main__':
-    #加载训练好的模型
-    model = YOLO(r'D:\marin program\yolov8\ultralytics\runs\detect\yolov5n\weights\best.pt')
-    # 对验证集进行评估
-    metrics = model.val(data = r'D:\marin program\yolov8\plant_data_new_new\DATA.yaml',batch=64)
