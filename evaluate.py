84ad830d26273019f0eb23579b33dae50bea0e53
MarinJH <148436558+MarinJH@users.noreply.github.com>
2025-07-09 23:43:11 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -4,6 +4,6 @@ import matplotlib
 matplotlib.use( "TkAgg")
 if __name__ == '__main__':
     #加载训练好的模型
-    model = YOLO(r'D:\marin program\yolov8\ultralytics\runs\detect\yolov5n\weights\best.pt')
+    model = YOLO(r'xxxxxx\best.pt')
     # 对验证集进行评估
-    metrics = model.val(data = r'D:\marin program\yolov8\plant_data_new_new\DATA.yaml',batch=64)
+    metrics = model.val(data = r'xxxxx\DATA.yaml',batch=64)
